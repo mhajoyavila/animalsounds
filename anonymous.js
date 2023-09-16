a2331dca71bed07c3ed141d7f9e13da394bf8eb0
Mary Joy Avila <maryjoyavila524@gmail.com>
2023-09-16 23:51:24 +0800

    new commits

--- anonymous.js
+++ anonymous.js
@@ -1,4 +1,5 @@
 // var numberOfAnimalButtons = document.querySelectorAll(".animal").length;
+
 // for (var i = 0; i < numberOfAnimalButtons; i++) {
 
 // document.querySelectorAll(".cat")[i].addEventListener("click", function() {
@@ -30,31 +31,46 @@
 var numberOfAnimalButtons = document.querySelectorAll(".animal").length;
 
 for (var i = 0; i <numberOfAnimalButtons; i++) {
-
     document.querySelectorAll(".animal")[i].addEventListener("click", function() {
-
         var buttons = this.innerText;
+        buttonAnimation(buttons)
+        playSound(buttons);
+    })
 
-        switch (buttons) {
-            case "cat":
+}
+    document.addEventListener("keydown", function (e) {
+        buttonAnimation(e.key)
+        playSound(e.key)
+        })
+
+        function playSound(key){
+        switch (key) {
+            case "c":
                 var cat = new Audio ("music/cat.mp3");
                 cat.play();
                 break;
-            case "doggy":
+            case "d":
                 var doggy = new Audio ("music/doggy.mp3");
                 doggy.play();
                 break;
-            case "giraffe":
+            case "g":
                 var giraffe = new Audio ("music/giraffe.mp3");
                 giraffe.play();
                 break;
-            case "piggy":
+            case "p":
                 var piggy = new Audio ("music/piggy.mp3");
                 piggy.play();
                 break;
             default:
+                console.log(key);
                 break;
         }
 
-});
-}
+    }
+    function buttonAnimation(currentKey) {
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function() {
+        activeButton.classList.remove("pressed");
+        }, 100);
+       }
